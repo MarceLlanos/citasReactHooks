489fc8d5e02cbceb0d19f5d2fdd4629badda6a04
Marcela Llanos <marcela3llanos@gmail.com>
2019-10-18 15:06:53 -0400

    Se hizo todo en una sola pagina

--- src/App.js
+++ src/App.js
@@ -1,10 +1,143 @@
-import React from 'react';
+import React, {useState, Fragment} from 'react';
+
+function Cita({cita}){
+  return (
+    <div className="cita">
+      <p>Mascota: <span>{cita.mascota}</span></p>
+      <p>Propietario: <span>{cita.propietario}</span></p>
+      <p>Fecha: <span>{cita.fecha}</span></p>
+      <p>Hora: <span>{cita.hora}</span></p>
+      <p>Sintomas: <span>{cita.sintomas}</span></p>
+    </div>
+  );
+}
+
+
+function Formulario({crearCita}){
+
+  const stateInicial = {
+    mascota:'',
+    propietario:'',
+    fecha: '',
+    hora:'',
+    sintomas:''
+  }
+
+  const [cita, actualizarCita] = useState(stateInicial)
+
+  const actualizarState = e =>{
+    actualizarCita({...cita, [e.target.name]: e.target.value})
+  }
+
+  const enviarCita = datos => {
+    datos.preventDefault();
+    //pasar la cita hacia el componente principal que es el App
+    crearCita(cita);
+
+    //Reiniciar el state(reinicial el form)
+    actualizarCita(stateInicial);
+  }
+
+  return(
+    <Fragment>
+      <h2>Crear Cita</h2>
+      <form onSubmit={enviarCita}>
+      <label>Nombre Mascota</label>
+      <input 
+        type="text" 
+        name="mascota"
+        className="u-full-width" 
+        placeholder="Nombre Mascota" 
+        onChange={actualizarState}
+        value={cita.mascota}
+      />
+
+      <label>Nombre Dueño</label>
+      <input 
+        type="text" 
+        name="propietario"
+        className="u-full-width"  
+        placeholder="Nombre Dueño de la Mascota" 
+        onChange={actualizarState}
+        value={cita.propietario}
+      />
+
+      <label>Fecha</label>
+      <input 
+        type="date" 
+        className="u-full-width"
+        name="fecha"
+        onChange={actualizarState}
+        value={cita.fecha}
+      />               
+
+      <label>Hora</label>
+      <input 
+        type="time" 
+        className="u-full-width"
+        name="hora" 
+        onChange={actualizarState}
+        value={cita.hora}
+      />
+
+      <label>Sintomas</label>
+      <textarea 
+        className="u-full-width"
+        name="sintomas"
+        onChange={actualizarState}
+        value={cita.sintomas}
+      ></textarea>
+
+      <button type="submit" className="button-primary u-full-width">Agregar</button>
+    </form>
+  </Fragment>
+  );
+}
 
 function App() {
+
+  /** 
+   * UseState retorna dos funciones
+   * El dato cita es el state actual = this.state
+   * El guardarCita (actualizaState) es una funcion que actualiza al state = this.setState()
+   * lo que esta dentro de useState(---Aqui---) es el valor inicial que tiene cita
+   */
+  const [citas, guardarCita] = useState([]);
+
+  //Agregar las nuevas citas al state
+  const crearCita = cita=> {
+
+    //tomar una copia del state y agregar el nuevo c
+    const nuevasCitas =[...citas, cita];
+
+    //Almacenamos en el state
+    guardarCita(nuevasCitas);
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <Fragment>
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+
+          <div className="one-half column">
+            <Formulario 
+              crearCita ={crearCita}
+            />
+          </div>
+
+          <div className="one-half column">
+            {citas.map((cita,index) => (
+              <Cita
+                key={index}
+                index={index}
+                cita={cita}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
